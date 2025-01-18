199e3b96f06cfb412dd81340dbe26967aba3adc9
Danil Zholobov <asusmemp10@gmail.com>
2025-01-19 02:28:53 +0300

    Создал програмку которая работает с new и delete в указателях

---
+++ управление памятью(new and delete)/управление памятью(new and delete).cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <iostream>
+#include <locale>
+#include <conio.h>
+using namespace std;
+
+class String {
+private:
+    char* str;
+public:
+    String(const char* FF) {
+        int a = strlen(FF) + 1;
+        str = new char[a];
+        strcpy(str, FF);
+    }
+    ~String() {
+        cout << "Удаляю строку... Прощайте друзья...";
+        delete[] str;
+    }
+    void display() {
+        cout << str << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const char* GG = "Вкусно и точка";
+    const char* GH = "С горы, с горы, с горы, я лечу";
+    String MMM(GG);
+    MMM.display();
+    String Azino(GH);
+    Azino.display();
+    _getch();
+}
