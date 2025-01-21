bd71fff76c28df73b95c889343bdd9e785262a3c
Danil Zholobov <asusmemp10@gmail.com>
2025-01-21 19:46:08 +0300

    Написал программу которая реализует связный список

---
+++ Связный список/Связный список.cpp
@@ -0,0 +1,47 @@
+﻿#include <conio.h>
+#include <iostream>
+#include <locale>
+using namespace std;
+
+struct list {
+    int data;
+    list* link;
+};
+
+class linklist {
+private:
+    list* stak;
+public:
+    linklist() {
+        stak = NULL;
+    }
+    void addition(int peak) {
+        list* temp = new list;
+        temp->data = peak;
+        temp->link = stak;
+        stak = temp;
+    }
+    void conclution() {
+        list* temp = stak;
+        while (temp) {
+            cout << temp->data << "\n";
+            temp = temp->link;
+        }
+    }
+};
+
+
+int main()
+{
+    linklist first;
+    first.addition(14);
+    first.addition(15);
+    first.addition(94);
+    first.addition(13);
+    first.addition(24);
+    first.conclution();
+    cout << endl;
+    first.conclution();
+    _getch();
+}
+
